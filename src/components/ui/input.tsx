7a984b839c8254658751b3d01794ad30dba9826a
vasylhulpak <hulpakvasyl@gmail.com>
2024-12-08 02:57:25 +0200

    Make a fix on uncontrolled input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,8 @@
-import * as React from "react";
-
 import { cn } from "@/utils/utils";
+import React from "react";
 
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-	({ className, type, ...props }, ref) => {
+	({ className, type, value, onChange, ...props }, ref) => {
 		return (
 			<input
 				type={type}
@@ -11,12 +10,15 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
 					"flex h-[48px] w-full border-black border-[1px] outline-none bg-white px-3 py-[10px] focus-visible:outline-none rounded-[0]",
 					className,
 				)}
+				value={value ?? ""} // Ensure the value is always defined (fallback to empty string)
+				onChange={onChange}
 				ref={ref}
 				{...props}
 			/>
 		);
 	},
 );
+
 Input.displayName = "Input";
 
 export { Input };
