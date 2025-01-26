8c6b8efc63c2f16e2a1c65555a657904ed9ad17f
vasylhulpak <hulpakvasyl@gmail.com>
2025-01-26 18:28:29 +0200

    Add small fix changes

--- src/components/dashboard/ShowTextBasedOnRoute.tsx
+++ src/components/dashboard/ShowTextBasedOnRoute.tsx
@@ -37,23 +37,11 @@ const ShowTextBasedOnRoute = () => {
 		return null;
 	}
 
-	function goToPrevious() {
-		// Split the URL into segments by '/'
-		const segments = pathname.split("/");
-
-		// Remove the last segment
-		segments.pop();
-
-		// Join the remaining segments back together
-		const baseUrl = segments.join("/");
-		router.push(baseUrl);
-	}
-
 	return (
 		<div className="text-[16px] leading-[18px] md:text-[24px] md:leading-[27px] font-[400] font-hagerman flex items-center justify-center h-[62px] border-b-[1px] border-grey30 relative">
 			<Button
 				className="absolute top-1/2 left-[16px] md:left-[32px] transform -translate-y-1/2 flex items-center gap-[8px] text-blue font-bold  text-[16px] leading-[18px] font-helvetica"
-				onClick={() => goToPrevious()}
+				onClick={() => router.back()}
 			>
 				<ChevronLeft className="text-blue w-[20px] h-[20px]" /> Back
 			</Button>
